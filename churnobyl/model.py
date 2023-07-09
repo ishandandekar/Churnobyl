9debded5a3e7088a188d097fd014a9977fd68c90
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-07-09 14:47:20 +0530

    Add base code

---
+++ churnobyl/model.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import typing as t
+import pandas as pd
+import xgboost as xgb
+from sklearn import (
+    model_selection,
+    preprocessing,
+    dummy,
+    metrics,
+    ensemble,
+    tree,
+    neighbors,
+    pipeline,
+    compose,
+    linear_model,
+    svm,
+)
+import optuna as opt
+
+
+def run_experiments(model_list):
+    ...
+
+
+def tune_model():
+    ...
