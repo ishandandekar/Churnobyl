3e92818cf0548be306d7c31142eeaaf4915a0847
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-07-09 20:13:21 +0530

    Add more base code

--- churnobyl/data.py
+++ churnobyl/data.py
@@ -2,8 +2,27 @@
 This script contains all the data utility functions.
 """
 
+from pathlib import Path
 import requests
 import pandas as pd
 import pandera as pa
+import boto3
 
+# TODO: Add code for schema
 SCHEMA = ...
+
+
+def load_csv_from_dir(dir: Path):
+    if dir.is_dir():
+        paths = dir.glob("*.csv")
+        for path in paths:
+            df = pd.read_csv(path)
+            # TODO: Validate dataframe
+        # TODO: Concat all dfs
+    else:
+        raise Exception("Path provided is not a directory")
+
+
+# TODO: Load from url
+def load_data_from_url(url: str):
+    ...
