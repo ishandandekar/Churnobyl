8f731ad2d9bc3c0a0423e3d02f6a502d1a05ae95
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-07-11 16:22:29 +0530

    Update code

--- data_schemas/schema_maker.py
+++ data_schemas/schema_maker.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import pandera as pa
-from data_schema import input_schema, churn_schema
+from data_schema import INPUT_SCHEMA, CHURN_SCHEMA
 
 with open("input_schema.yaml", "w") as file:
-    file.write(input_schema.to_yaml())
+    file.write(INPUT_SCHEMA.to_yaml())
 
 with open("churn_schema.yaml", "w") as file:
-    file.write(churn_schema.to_yaml())
+    file.write(CHURN_SCHEMA.to_yaml())
