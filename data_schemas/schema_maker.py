84ae4b7098afef1fbb8752863eeca77222332abe
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-07-14 19:05:01 +0530

    Update schemas

--- data_schemas/schema_maker.py
+++ data_schemas/schema_maker.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import pandera as pa
-from data_schema import INPUT_SCHEMA, CHURN_SCHEMA
+from data_schema import TRAINING_SCHEMA, INPUT_SCHEMA, CHURN_SCHEMA
+
+with open("training_schema.yaml", "w") as file:
+    file.write(TRAINING_SCHEMA.to_yaml())
 
 with open("input_schema.yaml", "w") as file:
     file.write(INPUT_SCHEMA.to_yaml())
