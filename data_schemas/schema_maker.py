f8ceae0d5ea91f2d842236250892c1f51baf800a
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-07-16 15:58:14 +0530

    Restucture data schemas

--- data_schemas/schema_maker.py
+++ data_schemas/schema_maker.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import pandera as pa
-from data_schema import CHURN_SCHEMA, INPUT_SCHEMA, TRAINING_SCHEMA
+from pathlib import Path
 
-with open("training_schema.yaml", "w") as file:
-    file.write(TRAINING_SCHEMA.to_yaml())
+data_schemas_path = Path.cwd() / "data_schemas"
+assert data_schemas_path.is_dir() == True
 
-with open("input_schema.yaml", "w") as file:
-    file.write(INPUT_SCHEMA.to_yaml())
+df = pd.read_csv("./data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
+training_schema = pa.infer_schema(df)
+churn_schema = pa.infer_schema(df[["Churn"]])
+input_schema = pa.infer_schema(df.drop(columns=["Churn"]))
 
-with open("churn_schema.yaml", "w") as file:
-    file.write(CHURN_SCHEMA.to_yaml())
+with open(data_schemas_path / "TRAINING_SCHEMA.py", "w") as file:
+    file.write(training_schema.to_script())
+with open(data_schemas_path / "CHURN_SCHEMA.py", "w") as file:
+    file.write(churn_schema.to_script())
+with open(data_schemas_path / "INPUT_SCHEMA.py", "w") as file:
+    file.write(input_schema.to_script())
