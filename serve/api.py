8cb54e079b01f6c2768e73934afd1a43a1059db3
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-07-20 21:54:49 +0530

    Add input schema

--- serve/api.py
+++ serve/api.py
@@ -1,4 +1,3 @@
-import json
 from datetime import datetime
 from functools import wraps
 import typing as t
@@ -9,11 +8,308 @@ import wandb
 import boto3
 from serve.schemas import PredictPayload
 from pathlib import Path
+from pandera import Check, Column, DataFrameSchema, Index, MultiIndex
 
-# TODO: Add code for FastAPI and then dockerize
-app = fastapi.FastAPI(
-    title="Churnobyl", description="Predict the churn, and DO NOT BURN", version="0.1"
+checks: t.Dict[str, t.List[Check]] = {
+    "customerID": [],
+    "gender": [Check.isin(["Male", "Female"])],
+    "SeniorCitizen": [Check.isin([0, 1])],
+    "Partner": [Check.isin(["Yes", "No"])],
+    "Dependents": [Check.isin(["Yes", "No"])],
+    "tenure": [
+        Check.greater_than_or_equal_to(min_value=0.0),
+        Check.less_than_or_equal_to(max_value=72.0),
+    ],
+    "PhoneService": [Check.isin(["No", "Yes"])],
+    "MultipleLines": [Check.isin(["No", "Yes", "No phone service"])],
+    "InternetService": [Check.isin(["DSL", "Fiber optic", "No"])],
+    "OnlineSecurity": [Check.isin(["No", "Yes", "No internet service"])],
+    "OnlineBackup": [Check.isin(["Yes", "No", "No internet service"])],
+    "DeviceProtection": [Check.isin(["No", "Yes", "No internet service"])],
+    "TechSupport": [Check.isin(["No", "Yes", "No internet service"])],
+    "StreamingTV": [Check.isin(["No", "Yes", "No internet service"])],
+    "StreamingMovies": [Check.isin(["No", "Yes", "No internet service"])],
+    "Contract": [Check.isin(["Month-to-month", "One year", "Two year"])],
+    "PaperlessBilling": [Check.isin(["Yes", "No"])],
+    "PaymentMethod": [
+        Check.isin(
+            [
+                "Electronic check",
+                "Mailed check",
+                "Bank transfer (automatic)",
+                "Credit card (automatic)",
+            ]
+        )
+    ],
+    "MonthlyCharges": [],
+    "TotalCharges": [],
+    "Churn": [Check.isin(["No", "Yes"])],
+}
+
+INPUT_SCHEMA = DataFrameSchema(
+    columns={
+        "customerID": Column(
+            dtype="object",
+            checks=checks.get("customerID"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "gender": Column(
+            dtype="object",
+            checks=checks.get("gender"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "SeniorCitizen": Column(
+            dtype="int64",
+            checks=checks.get("SeniorCitizen"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "Partner": Column(
+            dtype="object",
+            checks=checks.get("Partner"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "Dependents": Column(
+            dtype="object",
+            checks=checks.get("Dependents"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "tenure": Column(
+            dtype="int64",
+            checks=checks.get("tenure"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "PhoneService": Column(
+            dtype="object",
+            checks=checks.get("PhoneService"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "MultipleLines": Column(
+            dtype="object",
+            checks=checks.get("MultipleLines"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "InternetService": Column(
+            dtype="object",
+            checks=checks.get("InternetService"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "OnlineSecurity": Column(
+            dtype="object",
+            checks=checks.get("OnlineSecurity"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "OnlineBackup": Column(
+            dtype="object",
+            checks=checks.get("OnlineBackup"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "DeviceProtection": Column(
+            dtype="object",
+            checks=checks.get("DeviceProtection"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "TechSupport": Column(
+            dtype="object",
+            checks=checks.get("TechSupport"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "StreamingTV": Column(
+            dtype="object",
+            checks=checks.get("StreamingTV"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "StreamingMovies": Column(
+            dtype="object",
+            checks=checks.get("StreamingMovies"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "Contract": Column(
+            dtype="object",
+            checks=checks.get("Contract"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "PaperlessBilling": Column(
+            dtype="object",
+            checks=None,
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "PaymentMethod": Column(
+            dtype="object",
+            checks=checks.get("PaymentMethod"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "MonthlyCharges": Column(
+            dtype="float64",
+            checks=checks.get("MonthlyCharges"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+        "TotalCharges": Column(
+            dtype="object",
+            checks=checks.get("TotalCharges"),
+            nullable=False,
+            unique=False,
+            coerce=False,
+            required=True,
+            regex=False,
+            description=None,
+            title=None,
+        ),
+    },
+    checks=None,
+    index=Index(
+        dtype="int64",
+        checks=[],
+        nullable=False,
+        coerce=False,
+        name=None,
+        description=None,
+        title=None,
+    ),
+    dtype=None,
+    coerce=True,
+    strict=False,
+    name=None,
+    ordered=False,
+    unique=None,
+    report_duplicates="all",
+    unique_column_names=False,
+    title=None,
+    description=None,
 )
+
+
+def construct_response(f):
+    """Construct a JSON reponse for an endpoint"""
+
+    @wraps(f)
+    def wrap(request: fastapi.Request, *args, **kwargs) -> t.Dict:
+        results = f(request, *args, **kwargs)
+        response = {
+            "message": results["message"],
+            "method": request.method,
+            "status-code": results["status-code"],
+            "timestamp": datetime.now().isoformat(),
+            "url": request.url._url,
+        }
+        if "data" in results:
+            response["data"] = results["data"]
+        return response
+
+    return wrap
 
 
 def load_artifacts():
@@ -55,25 +351,10 @@ def unpickle(artifacts: t.Dict) -> t.Dict:
 artifacts = load_artifacts()
 artifacts = unpickle(artifacts)
 
-
-def construct_response(f):
-    """Construct a JSON reponse for an endpoint"""
-
-    @wraps(f)
-    def wrap(request: fastapi.Request, *args, **kwargs) -> t.Dict:
-        results = f(request, *args, **kwargs)
-        response = {
-            "message": results["message"],
-            "method": request.method,
-            "status-code": results["status-code"],
-            "timestamp": datetime.now().isoformat(),
-            "url": request.url._url,
-        }
-        if "data" in results:
-            response["data"] = results["data"]
-        return response
-
-    return wrap
+# TODO: Add code for FastAPI and then dockerize
+app = fastapi.FastAPI(
+    title="Churnobyl", description="Predict the churn, and DO NOT BURN", version="0.1"
+)
 
 
 @app.get("/")
@@ -107,6 +388,7 @@ def predict(request: fastapi.Request, payload: PredictPayload) -> t.Dict:
     """
     API endpoint to get predictions for one single data point
     """
+    # TODO: Validate data using TRAINING_SCHEMA
     # TODO: Log prediction to S3 bucket
     # TODO: Load Wandb artifacts
     ...
@@ -118,4 +400,10 @@ def flag():
     API endpoint to flag a prediction. Must contain the predicted label, prediction probability and the actual label
     """
     # TODO: Log flag to S3 bucket
+    ...
+
+
+# TODO: and this
+def data_reservoir():
+    # Add training samples to S3 bucket
     ...
