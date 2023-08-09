4381a70a37a9cc6183a8c5b983e62ff7b4e35ace
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-08-09 20:46:37 +0530

    Fix test for URL dataloader

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,66 @@
 """
 Tests for data module in churnobyl.data
 """
+import os
+import shutil
+from pathlib import Path
 import boto3
 import pandas as pd
-import requests
-
+from sklearn import datasets
+import pytest
 from churnobyl.data import DataDreamer
+
+
+def test_load_csv_from_dir():
+    iris = datasets.load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    DIR_PATH = "iris_data"
+    with pytest.raises(Exception) as e_info:
+        _ = DataDreamer.load_csv_from_dir(
+            dir=Path(DIR_PATH), columns=iris.feature_names
+        )
+
+    os.mkdir(DIR_PATH)
+
+    with pytest.raises(Exception) as e_info:
+        _ = DataDreamer.load_csv_from_dir(
+            dir=Path(DIR_PATH), columns=iris.feature_names
+        )
+
+    _df = df.head(50)
+    _ = df.to_csv("iris_data/iris_df.csv", index=False)
+    _ = _df.to_csv("iris_data/iris__df.csv", index=False)
+    data = DataDreamer.load_csv_from_dir(Path(DIR_PATH), columns=iris.feature_names)
+
+    assert data is not None
+    assert data.shape[0] == 200
+    assert data.columns.to_list() == iris.feature_names
+    shutil.rmtree(DIR_PATH)
+
+
+def test_load_csv_from_url():
+    URL_passes = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/monthly-mean-temp.csv"
+    URL_fails = "https://abc123.com"
+    COLS_passes = ["Month", "Temperature"]
+    COLS_fails = ["hi", "why are you looking here"]
+
+    with pytest.raises(Exception) as e_info:
+        _ = DataDreamer.load_csv_from_url(url=URL_fails, columns=COLS_passes)
+
+    with pytest.raises(Exception) as e_info:
+        _ = DataDreamer.load_csv_from_url(url=URL_fails, columns=COLS_fails)
+
+    df = DataDreamer.load_csv_from_url(url=URL_passes, columns=COLS_passes)
+
+    assert df is not None
+    assert df.shape[0] > 0
+
+
+def test_load_csv_from_aws_s3():
+    BUCKET_NAME = "churnobyl"
+    FOLDER_PATH = ""
+    SESSION = boto3.Session()
+    _ = DataDreamer.load_csv_from_aws_s3(
+        bucket_name=BUCKET_NAME, folder_path=FOLDER_PATH, session=SESSION
+    )
+    print(_)
