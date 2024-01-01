02beb785c35977746e8611e82892576a7ea21117
ishandandekar <ishandandekar2002@gmail.com>
2024-01-01 13:53:39 +0530

    Add improved model training and tuning engine

--- churnobyl/model.py
+++ churnobyl/model.py
@@ -3,67 +3,73 @@ This file contains functions and class to run modelling experiments,
 tune the hyperparameters and use shap values
 """
 
+from pathlib import Path
 import typing as t
+from dataclasses import dataclass
+from box import Box, BoxList
 import optuna
 import pandas as pd
-import numpy as np
 import xgboost as xgb
 from sklearn import ensemble, metrics, dummy, neighbors, linear_model, tree, svm, base
 from tqdm.auto import tqdm
 
 # DEV: Add models with names as key-value pair
-ModelFactory: t.Dict[str, t.Tuple[str, base.BaseEstimator]] = {
-    "dummy": (
-        "dummy",
-        dummy.DummyClassifier(strategy="most_frequent"),
-    ),
-    "knn": ("knn", neighbors.KNeighborsClassifier()),
-    "lr": (
-        "lr",
-        linear_model.LogisticRegression(solver="liblinear", class_weight="balanced"),
-    ),
-    "svm": ("svm", svm.SVC(kernel="rbf")),
-    "rf": ("rf", ensemble.RandomForestClassifier()),
-    "gb": (
-        "gb",
-        ensemble.GradientBoostingClassifier(),
-    ),
-    "dt": ("dt", tree.DecisionTreeClassifier()),
-    "abc": ("abc", ensemble.AdaBoostClassifier()),
-    "voting": (
-        "voting",
-        ensemble.VotingClassifier(estimators=[], voting="soft"),
-    ),
-    "xgb": ("xgb", xgb.XGBClassifier()),
+ModelFactory: t.Dict[str, t.Union[xgb.XGBClassifier, base.BaseEstimator]] = {
+    "dummy": dummy.DummyClassifier,
+    "knn": neighbors.KNeighborsClassifier,
+    "lr": linear_model.LogisticRegression,
+    "svm": svm.SVC,
+    "rf": ensemble.RandomForestClassifier,
+    "gb": ensemble.GradientBoostingClassifier,
+    "dt": tree.DecisionTreeClassifier,
+    "abc": ensemble.AdaBoostClassifier,
+    "voting": ensemble.VotingClassifier,
+    "xgb": xgb.XGBClassifier,
+    "ext": ensemble.ExtraTreesClassifier,
+    "sgd": linear_model.SGDClassifier,
 }
 
 
+@dataclass
+class TunerOutput:
+    studies: list[optuna.study.Study]
+    best_models: list[t.Union[base.BaseEstimator, xgb.XGBClassifier]]
+    best_parameters: list[dict]
+    best_metrics: list[float]
+    names: list[str]
+
+@dataclass
+class ModelEngineOutput:
+    results: pd.DataFrame
+    tuner: TunerOutput
+    paths: list[Path]
+
+
 class LearnLab:
-    # DEV: Add metrics if you want
     @staticmethod
-    def run_experiments(
-        model_list: list,
+    def train_experiments(
+        config: Box,
         X_train: pd.DataFrame,
         X_test: pd.DataFrame,
         y_train: pd.DataFrame,
         y_test: pd.DataFrame,
     ) -> pd.DataFrame:
-        """
-        Function to train a whole lot of models sequentially
-
-        Args:
-            model_list (list): List of models to train
-            X_train (pd.DataFrame): Features on which we want to train the model
-            X_test (pd.DataFrame): Features on which we want to test the model
-            y_train (pd.DataFrame): Target on which we want to train the model
-            y_test (pd.DataFrame): Target on which we want to test the model
-
-        Returns:
-            pd.DataFrame: DataFrame that contains the performance results for all the models in `model_list`
-        """
-
         results = dict()
-        for name, model in tqdm(model_list):
+        for idx in config.models:
+            model_name, model_params = list(idx.keys())[0], list(idx.values())[0].params
+            if model_name == "voting":
+                estimators_list = list()
+                for estimator in model_params.estimators:
+                    model_, model_params_ = (
+                        list(estimator.keys())[0],
+                        list(estimator.values())[0].params,
+                    )
+                    model__ = ModelFactory.get(model_)
+                    model__ = model__(**model_params_)
+                    estimators_list.append((model_, model__))
+                model_params["estimators"] = estimators_list
+            model = ModelFactory.get(model_name)
+            model = model(**model_params)
             model.fit(X_train, y_train)
             train_predictions = model.predict(X_train)
             test_predictions = model.predict(X_test)
@@ -81,7 +87,7 @@ class LearnLab:
             )
             test_recall = metrics.recall_score(y_true=y_test, y_pred=test_predictions)
             test_fscore = metrics.f1_score(y_true=y_test, y_pred=test_predictions)
-            results[name] = (
+            results[model_name] = (
                 train_accuracy,
                 train_precision,
                 train_recall,
@@ -108,141 +114,64 @@ class LearnLab:
 
         return results
 
-    # DEV: Add hyperparams for models
     @staticmethod
-    def tune_model(
+    def tune_models(
+        config: Box,
         X_train: pd.DataFrame,
         X_test: pd.DataFrame,
         y_train: pd.DataFrame,
         y_test: pd.DataFrame,
-        n_trials: int,
-    ) -> t.Tuple[
-        optuna.Study,
-        t.Union[ensemble.RandomForestClassifier, xgb.XGBClassifier],
-        t.Dict[str, t.Any],
-        t.Union[float, np.ndarray],
-        t.Union[t.Literal["RandomForest"], t.Literal["XGBoost"]],
-    ]:
-        """
-        Hyperparameter tuning using Optuna
-        """
+    ) -> TunerOutput:
+        studies, best_models, best_parameters, best_metrics, names = (
+            list(),
+            list(),
+            list(),
+            list(),
+            list(),
+        )
+        models: BoxList = config.get("models").to_list()
+        for model_param_item in models:
+            model_name, model_params = (
+                list(model_param_item.keys())[0],
+                list(model_param_item.values())[0].get("params"),
+            )
 
-        def tune_random_forest_clf(X_train, X_test, y_train, y_test, n_trials):
-            def objective(trial: optuna.Trial) -> float:
-                params = {
-                    "n_estimators": trial.suggest_int(
-                        "n_estimators", 100, 1000, step=100
-                    ),
-                    "max_depth": trial.suggest_int("max_depth", 3, 10),
-                    "min_samples_split": trial.suggest_int("min_samples_split", 2, 10),
-                    "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 10),
-                    "max_features": trial.suggest_categorical(
-                        "max_features", [None, "sqrt", "log2"]
-                    ),
-                    "random_state": 42,
-                }
-                model = ensemble.RandomForestClassifier(**params)
+            def objective(trial: optuna.Trial):
+                params = dict()
+                for k, v in model_params.items():
+                    args = v.get("args")
+                    args["name"] = k
+
+                    if v.get("strategy") == "float":
+                        params[k] = trial.suggest_float(**args)
+                    elif v.get("strategy") == "int":
+                        params[k] = trial.suggest_int(**args)
+                    elif v.get("strategy") == "cat":
+                        params[k] = trial.suggest_categorical(**args)
+                model = ModelFactory.get(model_name)
+                model = model(**params)
                 model.fit(X_train, y_train)
-                y_pred = model.predict(X_test)
-                auc = metrics.roc_auc_score(y_test, y_pred)
-                return auc
+                preds = model.predict(X_test)
+                return (metrics.f1_score(y_test, preds),)
 
+            optuna.logging.set_verbosity(optuna.logging.WARNING)
             study = optuna.create_study(direction="maximize")
-            study.optimize(objective, n_trials=n_trials)
-
+            study.optimize(objective, n_trials=config.get("n_trials"))
             best_params = study.best_params
-            best_model = ensemble.RandomForestClassifier(**best_params)
-            best_model.fit(X_train, y_train)
-            best_preds = best_model.predict(X_test)
-            best_metric = metrics.f1_score(y_test, best_preds)
-            return study, best_model, best_params, best_metric, "RandomForest"
-
-        def tune_xgboost_classifier(X_train, X_test, y_train, y_test, n_trials):
-            def objective(trial):
-                params = {
-                    "objective": "binary:logistic",
-                    "eval_metric": "auc",
-                    "eta": trial.suggest_float("eta", 0.01, 0.3, log=True),
-                    "max_depth": trial.suggest_int("max_depth", 3, 10),
-                    "subsample": trial.suggest_float("subsample", 0.5, 1.0),
-                    "colsample_bytree": trial.suggest_float(
-                        "colsample_bytree", 0.5, 1.0
-                    ),
-                    "lambda": trial.suggest_float("lambda", 0.01, 10.0, log=True),
-                    "alpha": trial.suggest_float("alpha", 0.01, 10.0, log=True),
-                    "min_child_weight": trial.suggest_int("min_child_weight", 1, 10),
-                    "early_stopping_rounds": 10,
-                    "callbacks": [
-                        optuna.integration.XGBoostPruningCallback(
-                            trial, "validation_0-auc"
-                        )
-                    ],
-                    "random_state": 42,
-                }
-
-                model = xgb.XGBClassifier(**params)
-
-                model.fit(
-                    X_train,
-                    y_train,
-                    eval_set=[(X_test, y_test)],
-                    verbose=False,
-                )
-
-                y_pred = model.predict(X_test)
-                auc = metrics.roc_auc_score(y_test, y_pred)
-                return auc
-
-            study = optuna.create_study(direction="maximize")
-            study.optimize(objective, n_trials=n_trials)
-
-            best_params = study.best_params
-            best_model = xgb.XGBClassifier(**best_params)
-
-            best_model.fit(X_train, y_train)
-            best_preds = best_model.predict(X_test)
-            best_metric = metrics.f1_score(y_test, best_preds)
-            return study, best_model, best_params, best_metric, "XGBoost"
-
-        (
-            rf_study,
-            rf_best_model,
-            rf_best_params,
-            rf_best_metric,
-            rf_type_,
-        ) = tune_random_forest_clf(
-            X_train=X_train,
-            X_test=X_test,
-            y_train=y_train,
-            y_test=y_test,
-            n_trials=n_trials,
-        )
-        (
-            xgb_study,
-            xgb_best_model,
-            xgb_best_params,
-            xgb_best_metric,
-            xgb_type_,
-        ) = tune_xgboost_classifier(
-            X_train=X_train,
-            X_test=X_test,
-            y_train=y_train,
-            y_test=y_test,
-            n_trials=n_trials,
-        )
-        if rf_best_metric > xgb_best_metric:
-            return (
-                rf_study,
-                rf_best_model,
-                rf_best_params,
-                rf_best_metric,
-                rf_type_,
+            best_model = ModelFactory.get(model_name)(**best_params).fit(
+                X_train, y_train
             )
-        else:
-            return (
-                xgb_study,
-                xgb_best_model,
-                xgb_best_params,
-                xgb_best_metric,
-                xgb_type_,
-            )
+            best_metric = study.best_value
+            studies.append(study)
+            best_models.append(best_model)
+            best_parameters.append(best_params)
+            best_metrics.append(best_metric)
+            names.append(model_name)
+        sorted_studies, sorted_best_models, sorted_best_parameters, sorted_best_metrics, sorted_names = zip(*sorted(zip(studies, best_metrics, best_parameters, best_metrics, names), key=lambda x: x[3], reverse=True)))
+        return TunerOutput(
+            studies=sorted_studies,
+            best_models=sorted_best_models,
+            best_parameters=sorted_best_parameters,
+            best_metrics=sorted_best_metrics,
+            names=sorted_names,
+        )
