99fd3186d285508c2b60d244966233a1b5cddcf8
ishandandekar <ishandandekar2002@gmail.com>
2024-01-02 23:50:43 +0530

    Add data engine template

--- churnobyl/data.py
+++ churnobyl/data.py
@@ -1,16 +1,16 @@
 """
 This script contains all the data utility functions.
 """
-from dataclasses import dataclass
-from abc import ABC, abstractmethod
 import typing as t
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from io import StringIO
 from pathlib import Path
+
 import boto3
 import pandas as pd
 import requests
 from pandera import Check, Column, DataFrameSchema, Index
-
 
 checks: t.Dict[str, t.List[Check]] = {
     "customerID": [],
@@ -47,290 +47,8 @@ checks: t.Dict[str, t.List[Check]] = {
     "Churn": [Check.isin(["No", "Yes"])],
 }
 
-CHURN_SCHEMA = DataFrameSchema(
-    columns={
-        "Churn": Column(
-            dtype="object",
-            checks=checks.get("Churn"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        )
-    },
-    checks=None,
-    index=Index(
-        dtype="int64",
-        checks=[],
-        nullable=False,
-        coerce=False,
-        name=None,
-        description=None,
-        title=None,
-    ),
-    dtype=None,
-    coerce=True,
-    strict=False,
-    name=None,
-    ordered=False,
-    unique=None,
-    report_duplicates="all",
-    unique_column_names=False,
-    title=None,
-    description=None,
-)
 
-
-INPUT_SCHEMA = DataFrameSchema(
-    columns={
-        "customerID": Column(
-            dtype="object",
-            checks=checks.get("customerID"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "gender": Column(
-            dtype="object",
-            checks=checks.get("gender"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "SeniorCitizen": Column(
-            dtype="int64",
-            checks=checks.get("SeniorCitizen"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "Partner": Column(
-            dtype="object",
-            checks=checks.get("Partner"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "Dependents": Column(
-            dtype="object",
-            checks=checks.get("Dependents"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "tenure": Column(
-            dtype="int64",
-            checks=checks.get("tenure"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "PhoneService": Column(
-            dtype="object",
-            checks=checks.get("PhoneService"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "MultipleLines": Column(
-            dtype="object",
-            checks=checks.get("MultipleLines"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "InternetService": Column(
-            dtype="object",
-            checks=checks.get("InternetService"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "OnlineSecurity": Column(
-            dtype="object",
-            checks=checks.get("OnlineSecurity"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "OnlineBackup": Column(
-            dtype="object",
-            checks=checks.get("OnlineBackup"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "DeviceProtection": Column(
-            dtype="object",
-            checks=checks.get("DeviceProtection"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "TechSupport": Column(
-            dtype="object",
-            checks=checks.get("TechSupport"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "StreamingTV": Column(
-            dtype="object",
-            checks=checks.get("StreamingTV"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "StreamingMovies": Column(
-            dtype="object",
-            checks=checks.get("StreamingMovies"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "Contract": Column(
-            dtype="object",
-            checks=checks.get("Contract"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "PaperlessBilling": Column(
-            dtype="object",
-            checks=None,
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "PaymentMethod": Column(
-            dtype="object",
-            checks=checks.get("PaymentMethod"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "MonthlyCharges": Column(
-            dtype="float64",
-            checks=checks.get("MonthlyCharges"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-        "TotalCharges": Column(
-            dtype="object",
-            checks=checks.get("TotalCharges"),
-            nullable=False,
-            unique=False,
-            coerce=False,
-            required=True,
-            regex=False,
-            description=None,
-            title=None,
-        ),
-    },
-    checks=None,
-    index=Index(
-        dtype="int64",
-        checks=[],
-        nullable=False,
-        coerce=False,
-        name=None,
-        description=None,
-        title=None,
-    ),
-    dtype=None,
-    coerce=True,
-    strict=False,
-    name=None,
-    ordered=False,
-    unique=None,
-    report_duplicates="all",
-    unique_column_names=False,
-    title=None,
-    description=None,
-)
-
-
-TRAINING_SCHEMA = DataFrameSchema(
+DataSchema = DataFrameSchema(
     columns={
         "customerID": Column(
             dtype="object",
@@ -587,7 +305,7 @@ TRAINING_SCHEMA = DataFrameSchema(
 )
 
 
-class DataLoaderStrategy(ABC):
+class BaseDataLoaderStrategy(ABC):
     @abstractmethod
     def __call__(self) -> pd.DataFrame:
         """
@@ -596,7 +314,7 @@ class DataLoaderStrategy(ABC):
 
 
 @dataclass
-class DirDataLoaderStrategy(DataLoaderStrategy):
+class DirDataLoaderStrategy(BaseDataLoaderStrategy):
     dir: t.Union[Path, str]
     columns: list[str]
 
@@ -617,7 +335,7 @@ class DirDataLoaderStrategy(DataLoaderStrategy):
 
 
 @dataclass
-class UrlDataLoaderStrategy(DataLoaderStrategy):
+class UrlDataLoaderStrategy(BaseDataLoaderStrategy):
     url: str
     columns: list[str]
 
@@ -634,7 +352,7 @@ class UrlDataLoaderStrategy(DataLoaderStrategy):
 
 
 @dataclass
-class AwsS3DataLoaderStrategy(DataLoaderStrategy):
+class AwsS3DataLoaderStrategy(BaseDataLoaderStrategy):
     bucket_name: str
     folder_path: str
     session: boto3.Session
@@ -670,8 +388,27 @@ class AwsS3DataLoaderStrategy(DataLoaderStrategy):
         return pd.concat(dataframes, ignore_index=True)
 
 
-DataLoaderStrategyFactory: t.Dict[str, t.Type[DataLoaderStrategy]] = {
+DataLoaderStrategyFactory: t.Dict[str, t.Type[BaseDataLoaderStrategy]] = {
     "dir": DirDataLoaderStrategy,
     "url": UrlDataLoaderStrategy,
     "aws_s3": AwsS3DataLoaderStrategy,
 }
+
+
+# TODO: Write data engine using polars
+class DataEngine:
+    @staticmethod
+    def load(config) -> pd.DataFrame:
+        ...
+
+    @staticmethod
+    def validate(schema):
+        ...
+
+    @staticmethod
+    def split():
+        ...
+
+    @staticmethod
+    def transform():
+        ...
