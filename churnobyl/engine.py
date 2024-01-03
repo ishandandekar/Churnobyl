1fcf73bc4126d57c23e745363d91f3cdfedef12e
ishandandekar <ishandandekar2002@gmail.com>
2024-01-03 16:33:20 +0530

    Update for new data engine and split training and tuning

--- churnobyl/engine.py
+++ churnobyl/engine.py
@@ -8,25 +8,17 @@ import random
 import typing as t
 from pathlib import Path
 
-import cloudpickle as cpickle
 import numpy as np
-import numpy.typing as npt
 import optuna
-import pandas as pd
-import pandera as pa
+import polars as pl
 import wandb
 import yaml
 from box import Box
 from prefect import artifacts, flow, get_run_logger, task
-from sklearn import model_selection, preprocessing
 
-from data import TRAINING_SCHEMA, DataLoaderStrategyFactory
-from model import LearnLab, ModelEngineOutput
+from data import DataEngine, TransformerOutput
+from model import LearnLab, TunerOutput
 from visualize import Vizard
-
-
-def _custom_combiner(feature, category):
-    return str(feature) + "_" + type(category).__name__ + "_" + str(category)
 
 
 @task(
@@ -48,12 +40,7 @@ def set_config(config_path: Path) -> Box:
     """
     if config_path.exists():
         with open(config_path, "r") as stream:
-            try:
-                config = yaml.safe_load(stream)
-                return Box(config)
-            except yaml.YAMLError as exc:
-                print(exc)
-                return None
+            return Box(yaml.safe_load(stream=stream))
     else:
         raise Exception("Path error occured. File does not exist")
 
@@ -99,7 +86,7 @@ def setup_pipeline(
     retries=3,
     retry_delay_seconds=3,
 )
-def data_loader(config: Box, schema: pa.DataFrameSchema) -> pd.DataFrame:
+def data_loader(config: Box) -> pl.DataFrame:
     """
     Loads data from functions mentioned by the developer,
     Also validates data based on schema
@@ -114,19 +101,11 @@ def data_loader(config: Box, schema: pa.DataFrameSchema) -> pd.DataFrame:
     Returns:
         pd.DataFrame: concatenated data that now be split into train and test sets
     """
-    data: pd.DataFrame = DataLoaderStrategyFactory.get(config.data.load.strategy)(
-        **config.data.load.args
-    )()
-    data["TotalCharges"] = data["TotalCharges"].replace(to_replace=" ", value="0")
-    try:
-        schema.validate(data, lazy=True)
-    except pa.errors.SchemaErrors as err:
-        print("Schema errors and failure cases:")
-        print(err.failure_cases)
-        print("\nDataFrame object that failed validation:")
-        print(err.data)
-        raise Exception("Schema errors and failure cases")
-    return data
+    return DataEngine.load(config.data.load)
+
+
+def data_validator(data) -> pl.DataFrame:
+    return DataEngine.validate(data)
 
 
 @task(
@@ -134,8 +113,8 @@ def data_loader(config: Box, schema: pa.DataFrameSchema) -> pd.DataFrame:
     description="Split data into training and test sets",
 )
 def data_splits(
-    config: Box, df: pd.DataFrame
-) -> t.Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    config: Box, data: pl.DataFrame
+) -> t.Tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame, pl.DataFrame]:
     """
     Splits the data into train and test sets
 
@@ -146,24 +125,7 @@ def data_splits(
     Returns:
         Training features, test features, train labels, test labels
     """
-    X, y = df.drop(columns=["Churn"]), df[["Churn"]]
-    if config.data.split.stratify:
-        X_train, X_test, y_train, y_test = model_selection.train_test_split(
-            X,
-            y,
-            test_size=config.data.split.ratio,
-            random_state=config.SEED,
-            stratify=y,
-        )
-    else:
-        X_train, X_test, y_train, y_test = model_selection.train_test_split(
-            X,
-            y,
-            test_size=config.data.split.ratio,
-            random_state=config.SEED,
-        )
-
-    return X_train, X_test, y_train, y_test
+    return DataEngine.split(config.data.split, data=data)
 
 
 @task(
@@ -172,143 +134,42 @@ def data_splits(
 )
 def data_transformer(
     config: Box,
-    X_train: pd.DataFrame,
-    X_test: pd.DataFrame,
-    y_train: pd.DataFrame,
-    y_test: pd.DataFrame,
+    X_train: pl.DataFrame,
+    X_test: pl.DataFrame,
+    y_train: pl.DataFrame,
+    y_test: pl.DataFrame,
     artifact_dir: Path,
-) -> t.Tuple[pd.DataFrame, pd.DataFrame, npt.ArrayLike, npt.ArrayLike]:
+) -> TransformerOutput:
     """
     Applies transformation like scaling and encoding to data
     """
-    scaler = preprocessing.StandardScaler().set_output(transform="pandas")
-    encoder_ohe = preprocessing.OneHotEncoder(feature_name_combiner=_custom_combiner)
-    encoder_oe = preprocessing.OrdinalEncoder()
-    target_encoder = preprocessing.LabelEncoder()
-
-    # One-hot encoding
-    X_ohe__train = X_train[config.data.get("CAT_COLS_OHE")]
-    encoder_ohe.fit(X_ohe__train)
-
-    X_ohe_trans__train = encoder_ohe.transform(X_ohe__train)
-    X_ohe_trans_df__train: pd.DataFrame = pd.DataFrame(
-        X_ohe_trans__train.toarray(), columns=encoder_ohe.get_feature_names_out()
+    return DataEngine.transform(
+        config=config.data.transform,
+        X_train=X_train,
+        X_test=X_test,
+        y_train=y_train,
+        y_test=y_test,
+        artifact_dir=artifact_dir,
     )
-    X_ohe__test = X_test[config.data.get("CAT_COLS_OHE")]
-    X_ohe_trans__test = encoder_ohe.transform(X_ohe__test)
-    X_ohe_trans__test: pd.DataFrame = pd.DataFrame(
-        X_ohe_trans__test.toarray(), columns=encoder_ohe.get_feature_names_out()
-    )
-
-    # Ordinal Encoder
-    X_oe__train = X_train[config.data.get("CAT_COLS_OE")]
-    encoder_oe.fit(X_oe__train.values)
-
-    X_oe_trans__train = encoder_oe.transform(X_oe__train.values)
-    X_oe_trans__train = pd.DataFrame(
-        X_oe_trans__train, columns=config.data.get("CAT_COLS_OE")
-    )
-    X_oe__test = X_test[config.data.get("CAT_COLS_OE")]
-    X_oe_trans__test = encoder_oe.transform(X_oe__test.values)
-    X_oe_trans__test: pd.DataFrame = pd.DataFrame(
-        X_oe_trans__test, columns=config.data.get("CAT_COLS_OE")
-    )
-
-    # Scale
-    X_scale__train = X_train[config.data.get("NUM_COLS")]
-    scaler.fit(X_scale__train)
-
-    X_scale_trans__train: pd.DataFrame = scaler.transform(X_scale__train)
-    X_scale__test = X_test[config.data.get("NUM_COLS")]
-    X_scale_trans__test: pd.DataFrame = scaler.transform(X_scale__test)
-
-    # Encode target variable
-    target_encoder.fit(y_train)
-
-    y_to_train = target_encoder.transform(y_train)
-    y_to_test = target_encoder.transform(y_test)
-
-    X_to_train: pd.DataFrame = pd.concat(
-        [
-            X_ohe_trans_df__train.reset_index(drop=True),
-            X_oe_trans__train.reset_index(drop=True),
-            X_scale_trans__train.reset_index(drop=True),
-        ],
-        axis=1,
-    )
-    X_to_test: pd.DataFrame = pd.concat(
-        [
-            X_ohe_trans__test.reset_index(drop=True),
-            X_oe_trans__test.reset_index(drop=True),
-            X_scale_trans__test.reset_index(drop=True),
-        ],
-        axis=1,
-    )
-
-    # Saving preprocessors as `.pkl` files
-    preprocessors_names = [
-        "encoder_ohe_",
-        "encoder_oe_",
-        "scaler_standard_",
-        "target_encoder_",
-    ]
-    preprocessors = [encoder_ohe, encoder_oe, scaler, target_encoder]
-    for name, preprocessor in zip(preprocessors_names, preprocessors):
-        path_ = artifact_dir / f"{name}.pkl"
-        with open(str(path_), "wb") as f:
-            cpickle.dumps(preprocessor, f)
-
-    return X_to_train, X_to_test, y_to_train, y_to_test
 
 
 @task(
-    name="make_models",
-    description="Fit multiple models on data and tune best models using optuna",
-    retries=1,
+    name="train_models",
+    description="Trains model based on params, returns a dataframe containing metrics",
 )
-def model_engine(
-    config: Box,
-    X_train: pd.DataFrame,
-    X_test: pd.DataFrame,
-    y_train: npt.ArrayLike,
-    y_test: npt.ArrayLike,
-    model_dir: Path,
-) -> ModelEngineOutput:
-    """
-    Trains and tunes models based on `args: config`
-
-    Args:
-        config (Box): Configuration mapping for training and tuning
-        X_train (pd.DataFrame): Training features
-        X_test (pd.DataFrame): Test features
-        y_train (npt.ArrayLike): Training labels
-        y_test (npt.ArrayLike): Test labels
-        model_dir (Path): Directory to store tuned models
-
-    Returns:
-        ModelEngineOutput: Output class for training results, tuner output and paths for the tuned models
-    """
-    results: pd.DataFrame = LearnLab.train_experiments(
-        config=config.model.train,
-        X_train=X_train,
-        X_test=X_test,
-        y_train=y_train,
-        y_test=y_test,
+def train_models(config: Box, transformed_ds: TransformerOutput) -> pl.DataFrame:
+    return LearnLab.train_experiments(
+        config=config.model.train, transformed_ds=transformed_ds
     )
-    tuner = LearnLab.tune_models(
-        config=config.model.tune,
-        X_train=X_train,
-        X_test=X_test,
-        y_train=y_train,
-        y_test=y_test,
+
+
+@task
+def tune_models(
+    config: Box, transformed_ds: t.Type[TransformerOutput], model_dir: Path
+) -> TunerOutput:
+    return LearnLab.tune_models(
+        config=config.model.tune, transformed_ds=transformed_ds, model_dir=model_dir
     )
-    best_paths = list()
-    for model, name in zip(tuner.best_models, tuner.names):
-        path_ = model_dir / f"{name}_tuned_.pkl"
-        with open(path_, "wb") as f_out:
-            cpickle.dump(model, f_out)
-        best_paths.append(path_)
-    return ModelEngineOutput(results=results, tuner=tuner, paths=best_paths)
 
 
 @task(
@@ -316,8 +177,8 @@ def model_engine(
     description="Create visualizations for model explainability and data analysis",
 )
 def vizard(
-    df: pd.DataFrame,
-    results: pd.DataFrame,
+    df: pl.DataFrame,
+    results: pl.DataFrame,
     study: optuna.Study,
     viz_dir: Path,
 ) -> None:
@@ -420,11 +281,11 @@ def main_workflow(config_path: Path) -> None:
     ) = setup_pipeline(config=config)
     logger = get_run_logger()
     logger.info("Setting up directories and logging")
-    df = data_loader(config=config, schema=TRAINING_SCHEMA)
+    df = data_loader(config=config)
     logger.info("Data has been loaded")
     X_train, X_test, y_train, y_test = data_splits(config=config, df=df)
     logger.info("Data splits have been made")
-    X_to_train, X_to_test, y_to_train, y_to_test = data_transformer(
+    transformed_ds = data_transformer(
         config=config,
         X_train=X_train,
         X_test=X_test,
@@ -433,28 +294,25 @@ def main_workflow(config_path: Path) -> None:
         artifact_dir=ARTIFACT_DIR,
     )
     logger.info("Data transformers have been applied")
-    model_engined_output = model_engine(
-        config=config,
-        X_train=X_to_train,
-        X_test=X_to_test,
-        y_train=y_to_train,
-        y_test=y_to_test,
-        model_dir=MODEL_DIR,
+
+    results = train_models(config=config, transformed_ds=transformed_ds)
+    tuned = tune_models(
+        config=config, transformed_ds=transformed_ds, model_dir=MODEL_DIR
     )
     logger.info("Best model has been acquired")
     _ = vizard(
         df=df,
-        results=model_engined_output.results,
-        study=model_engined_output.tuner.studies[0],
-        model=model_engined_output.tuner.best_models[0],
-        X_train=X_to_train,
+        results=results,
+        study=tuned.studies[0],
+        model=tuned.best_models[0],
+        X_train=transformed_ds.X_train,
         viz_dir=VIZ_DIR,
     )
     logger.info("Visualizations have been drawn")
     _ = push_artifacts(
-        best_type_=model_engined_output.tuner.names[0],
-        best_metric=model_engined_output.tuner.best_metrics[0],
-        best_path_=model_engined_output.paths[0],
+        best_type_=tuned.names[0],
+        best_metric=tuned.best_metrics[0],
+        best_path_=tuned.best_paths[0],
         artifact_dir=ARTIFACT_DIR,
         viz_dir=VIZ_DIR,
     )
