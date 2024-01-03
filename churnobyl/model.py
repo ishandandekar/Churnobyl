e266fc468563bfbc1a6f8cc708984eb6c77acde6
ishandandekar <ishandandekar2002@gmail.com>
2024-01-03 16:33:39 +0530

    Bump to polars

--- churnobyl/model.py
+++ churnobyl/model.py
@@ -9,9 +9,12 @@ from pathlib import Path
 
 import optuna
 import pandas as pd
+import polars as pl
 import xgboost as xgb
 from box import Box, BoxList
 from sklearn import base, dummy, ensemble, linear_model, metrics, neighbors, svm, tree
+import cloudpickle as cpickle
+from data import TransformerOutput
 
 # DEV: Add models with names as key-value pair
 ModelFactory: t.Dict[str, t.Union[xgb.XGBClassifier, base.BaseEstimator]] = {
@@ -37,25 +40,23 @@ class TunerOutput:
     best_parameters: list[dict]
     best_metrics: list[float]
     names: list[str]
-
-
-@dataclass
-class ModelEngineOutput:
-    results: pd.DataFrame
-    tuner: TunerOutput
-    paths: list[Path]
+    best_paths: list[Path]
 
 
 class LearnLab:
     @staticmethod
     def train_experiments(
-        config: Box,
-        X_train: pd.DataFrame,
-        X_test: pd.DataFrame,
-        y_train: pd.DataFrame,
-        y_test: pd.DataFrame,
-    ) -> pd.DataFrame:
-        results = dict()
+        config: Box, transformed_ds: TransformerOutput
+    ) -> pl.DataFrame:
+        X_train, X_test, y_train, y_test = (
+            transformed_ds.X_train,
+            transformed_ds.X_test,
+            transformed_ds.y_train,
+            transformed_ds.y_test,
+        )
+        results: t.Dict[
+            str, t.Tuple[float, float, float, float, float, float, float, float]
+        ] = dict()
         for idx in config.models:
             model_name, model_params = list(idx.keys())[0], list(idx.values())[0].params
             if model_name == "voting":
@@ -98,37 +99,51 @@ class LearnLab:
                 test_recall,
                 test_fscore,
             )
-        results: pd.DataFrame = pd.DataFrame.from_dict(
-            results,
-            orient="index",
-            columns=[
-                "train_accuracy",
-                "train_precision",
-                "train_recall",
-                "train_fscore",
-                "test_accuracy",
-                "test_precision",
-                "test_recall",
-                "test_fscore",
-            ],
-        ).sort_values(by=["test_fscore"], ascending=False)
-
-        return results
+        return (
+            pl.LazyFrame(results)
+            .collect()
+            .transpose()
+            .lazy()
+            .with_columns(
+                pl.col("column_0").alias("train_accuracy"),
+                pl.col("column_1").alias("train_precision"),
+                pl.col("column_2").alias("train_recall"),
+                pl.col("column_3").alias("train_fscore"),
+                pl.col("column_4").alias("test_accuracy"),
+                pl.col("column_5").alias("test_precision"),
+                pl.col("column_6").alias("test_recall"),
+                pl.col("column_7").alias("test_fscore"),
+            )
+            .select(
+                pl.col("train_accuracy"),
+                pl.col("train_precision"),
+                pl.col("train_recall"),
+                pl.col("train_fscore"),
+                pl.col("test_accuracy"),
+                pl.col("test_precision"),
+                pl.col("test_recall"),
+                pl.col("test_fscore"),
+            )
+            .collect()
+        )
 
     @staticmethod
     def tune_models(
-        config: Box,
-        X_train: pd.DataFrame,
-        X_test: pd.DataFrame,
-        y_train: pd.DataFrame,
-        y_test: pd.DataFrame,
+        config: Box, transformed_ds: t.Type[TransformerOutput], model_dir: Path
     ) -> TunerOutput:
-        studies, best_models, best_parameters, best_metrics, names = (
+        studies, best_models, best_parameters, best_metrics, names, best_paths = (
             list(),
             list(),
             list(),
             list(),
             list(),
+            list(),
+        )
+        X_train, X_test, y_train, y_test = (
+            transformed_ds.X_train,
+            transformed_ds.X_test,
+            transformed_ds.y_train,
+            transformed_ds.y_test,
         )
         models: BoxList = config.get("models").to_list()
         for model_param_item in models:
@@ -163,20 +178,33 @@ class LearnLab:
                 X_train, y_train
             )
             best_metric = study.best_value
+
+            path_ = model_dir / model_name
+            with open(path_, "wb") as f_out:
+                cpickle.dump(best_model, f_out)
             studies.append(study)
             best_models.append(best_model)
             best_parameters.append(best_params)
             best_metrics.append(best_metric)
             names.append(model_name)
+            best_paths.append(path_)
         (
             sorted_studies,
             sorted_best_models,
             sorted_best_parameters,
             sorted_best_metrics,
             sorted_names,
+            sorted_paths,
         ) = zip(
             *sorted(
-                zip(studies, best_metrics, best_parameters, best_metrics, names),
+                zip(
+                    studies,
+                    best_metrics,
+                    best_parameters,
+                    best_metrics,
+                    names,
+                    best_paths,
+                ),
                 key=lambda x: x[3],
                 reverse=True,
             )
@@ -187,4 +215,5 @@ class LearnLab:
             best_parameters=sorted_best_parameters,
             best_metrics=sorted_best_metrics,
             names=sorted_names,
+            best_paths=sorted_paths,
         )
