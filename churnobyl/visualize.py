31463639a06e60aeebb2dc6c4acf5a43333a3c40
ishandandekar <ishandandekar2002@gmail.com>
2024-01-10 17:05:27 +0530

    Add new visualizations

--- churnobyl/visualize.py
+++ churnobyl/visualize.py
@@ -1,13 +1,16 @@
 """
 Contains functions to make plots and visualizations
 """
+import typing as t
 import warnings
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import optuna
 import polars as pl
 import seaborn as sns
+from box import Box
 
 warnings.simplefilter("ignore")
 plt.switch_backend("agg")
@@ -15,7 +18,6 @@ plt.ioff()
 
 
 class Vizard:
-    # DEV: Add `optuna.visualizations` as per need
     @staticmethod
     def plot_optuna_study(
         study: optuna.Study,
@@ -27,11 +29,11 @@ class Vizard:
         return None
 
     @staticmethod
-    def plot_target_dist(df: pl.DataFrame, directory: Path) -> None:
+    def plot_target_dist(data: pl.DataFrame, directory: Path) -> None:
         fig = plt.figure(figsize=(10, 6))
         ax = fig.add_subplot(111)
         churn_response = (
-            df.select(pl.col("Churn"))
+            data.select(pl.col("Churn"))
             .to_series()
             .value_counts(sort=True, parallel=True)
         )
@@ -54,7 +56,7 @@ class Vizard:
         return None
 
     @staticmethod
-    def plot_cust_info(df: pl.DataFrame, viz_dir: Path) -> None:
+    def plot_cust_info(data: pl.DataFrame, viz_dir: Path) -> None:
         colors = ["#E94B3C", "#2D2926"]
         l1 = ["gender", "SeniorCitizen", "Partner", "Dependents"]
         _ = plt.subplots(nrows=2, ncols=2, figsize=(12, 10))
@@ -62,7 +64,7 @@ class Vizard:
             plt.subplot(2, 2, i + 1)
             ax = sns.countplot(
                 x=l1[i],
-                data=df.to_pandas(),
+                data=data.to_pandas(),
                 hue="Churn",
                 palette=colors,
                 edgecolor="black",
@@ -83,14 +85,46 @@ class Vizard:
         return None
 
     @staticmethod
-    def plot_num_dist(df: pl.DataFrame, viz_dir: Path) -> None:
+    def plot_num_dist(data: pl.DataFrame, viz_dir: Path) -> None:
         num_features = ["tenure", "MonthlyCharges", "TotalCharges"]
         _, _ = plt.subplots(nrows=1, ncols=3, figsize=(11, 5))
         for i in range(len(num_features)):
             plt.subplot(1, 3, i + 1)
-        sns.distplot(df[num_features[i]], color="#778da9")
+        sns.distplot(data[num_features[i]], color="#778da9")
         title = "Distribution : " + num_features[i]
         plt.title(title)
         plt.savefig(viz_dir / "num_cols_dist.png", format="png")
         plt.close()
         return None
+
+    @staticmethod
+    def plot_training_results(config: Box, results: pl.DataFrame, viz_dir: Path):
+        arr_labels = results.columns
+        results: t.Dict[str, t.List[float]] = results.transpose().to_dict(
+            as_series=False
+        )
+        models_lst = [
+            list(model_item.keys())[0] for model_item in config.model.train.models
+        ]
+        values = list(results.values())
+        num_bars = len(values[0])
+
+        positions = np.arange(len(models_lst))
+        group_width = 0.4
+        width = 0.2
+        plt.figure(figsize=(22, 7))
+        for i, label in zip(range(num_bars), arr_labels):
+            plt.bar(
+                positions + i * group_width,
+                [v[i] for v in values],
+                width=width,
+                label=label,
+            )
+
+        plt.xlabel("Models")
+        plt.ylabel("Metrics")
+        plt.title("Training results")
+        plt.xticks(positions + width * (num_bars + 1) / 2, models_lst)
+        plt.legend()
+        plt.savefig(viz_dir / "training_results.png", format="png")
+        plt.close()
