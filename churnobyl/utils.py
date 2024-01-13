691bd1f400891bc5d2004b8824bfaeb08b1887d6
ishandandekar <ishandandekar2002@gmail.com>
2024-01-13 23:22:44 +0530

    Add data config validation

--- churnobyl/utils.py
+++ churnobyl/utils.py
@@ -7,11 +7,15 @@ import yaml
 from box import Box
 from exceptions import ConfigValidationError
 
+from data import validate_data_config
+
 
 class Pilot:
     @staticmethod
-    def __validate_config(config):
-        raise ConfigValidationError("ERROR RAISED")
+    def __validate_config(config: Box):
+        if not isinstance(config.get("SEED"), int):
+            raise ConfigValidationError("SEED should be an integer.")
+        validate_data_config(config=config)
 
     @staticmethod
     def setup(filepath: t.Union[str, Path]) -> t.Tuple[Box, Path, Path, Path, Path]:
@@ -20,7 +24,9 @@ class Pilot:
         if filepath.exists():
             with open(filepath, "r") as f_in:
                 config = Box(yaml.safe_load(stream=f_in))
-            # Pilot.__validate_config(config=config)
+
+            Pilot.__validate_config(config=config)
+
             ROOT_DIR = Path.cwd()
             VIZ_DIR: Path = ROOT_DIR / config.path.viz
             MODEL_DIR: Path = ROOT_DIR / config.path.model
@@ -39,3 +45,7 @@ class Pilot:
                 MODEL_DIR,
                 ARTIFACT_DIR,
             )
+
+
+if __name__ == "__main__":
+    tup = Pilot.setup("./churnobyl/conf/config.yaml")
