1c851817375e6d7f8efa0d63e59c3b2069cde7a6
ishandandekar <ishandandekar2002@gmail.com>
2024-01-29 10:01:56 +0530

    Add temp

--- temp/check_from_mlflow.py
+++ temp/check_from_mlflow.py
@@ -2,11 +2,11 @@ from pprint import pprint
 
 import mlflow
 
-runs = mlflow.search_runs(["1"], order_by=["end_time"]).to_dict()
+runs = mlflow.search_runs(["1"], order_by=["end_time"])
 pprint(runs)
-# print(runs)
-# run_id = runs.iloc[-1, :]["run_id"]
-# with mlflow.start_run(run_id=run_id):
-#     m = mlflow.sklearn.load_model(f"s3://mlflow-churnobyl/1/{run_id}/artifacts/model")
-#     print(type(m))
-#     print(m)
+run_id = runs.iloc[-1, :]["run_id"]
+print(run_id)
+with mlflow.start_run(run_id=run_id):
+    m = mlflow.sklearn.load_model(f"s3://mlflow-churnobyl/1/{run_id}/artifacts/model")
+    print(type(m))
+    print(m)
